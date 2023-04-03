e4d50757178b7a102c0b7c8571ef8b74e62d6c09
skrypnikaw <skrypnikaw@gmail.com>
2023-04-03 21:28:31 +0300

    Редакция бага с инициализацией фрейма и страницы калькулятора. Теперь все работает как нужно)

--- Views/Base/MainWindow.xaml.cs
+++ Views/Base/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NinjaWorld_Encyclopedia.Views.Pages.Calculator;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NinjaWorld_Encyclopedia
 {
@@ -23,6 +11,7 @@ namespace NinjaWorld_Encyclopedia
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Content = new CalculatorBase();
         }
     }
 }
