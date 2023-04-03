6a9ea1fb1a3b596965e76b63b057f3b617f0ad99
skrypnikaw <skrypnikaw@gmail.com>
2023-04-03 20:05:36 +0300

    Создание ActionCommand

---
+++ Infastructure/Commands/ActionCommand.cs
@@ -0,0 +1,22 @@
+﻿using NinjaWorld_Encyclopedia.Infastructure.Commands.Base;
+using System;
+
+namespace NinjaWorld_Encyclopedia.Infastructure.Commands
+{
+    internal class ActionCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public ActionCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _canExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _execute(parameter);
+
+    }
+}
---
+++ Infastructure/Commands/Base/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace NinjaWorld_Encyclopedia.Infastructure.Commands.Base
+{
+    /* Создание базового абстрактного класса интерфейса ICommand */
+
+    internal abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+
+
+        public abstract void Execute(object parameter);
+
+    }
+}
