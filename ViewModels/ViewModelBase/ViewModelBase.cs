ed99553f25ea5b43db6d53316584a101a3f15b44
skrypnikaw <skrypnikaw@gmail.com>
2023-04-03 19:56:51 +0300

    Создание Интерфейса INotifyPropertyChanged в базовый абстрактный класс

---
+++ ViewModels/ViewModelBase/ViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NinjaWorld_Encyclopedia.ViewModels.ViewModelBase
+{
+    /* Создание Интерфейса INotifyPropertyChanged в базовый абстрактный класс*/
+
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
